9d630fcd639a6738734ae605ce4a448f1f7ede4f
Neos21 <neos21@gmail.com>
2020-09-08 19:13:36 +0900

    WIP

--- bin/hb.js
+++ bin/hb.js
@@ -2,32 +2,43 @@
 
 const args = process.argv.slice(2, process.argv.length);
 
-if(!args.length) {
-  console.error('No args');
-  return process.exit(1);
+if(!args.length) return console.log('No args');
+
+if(args[0] === 'init') return require('../lib/init')();
+
+const config = require('../lib/load-config-file')();
+if(!config || !require('../lib/validate-config')(config)) return;
+
+const profile = require('../lib/detect-profile')(args, config);
+if(!profile) return;
+
+if((args[0] === 'get' && ['draft', 'drafts'].includes(args[1])) || args[0] === 'gd') {
+  return (async () => { return await require('../lib/get-draft')(config, profile); })();
 }
 
-if(args[0] === 'init') {
-  console.log('Init');
-}
-else if((args[0] === 'get' && ['draft', 'drafts'].includes(args[1])) || args[0] === 'gd') {
-  console.log('Get Draft');
-}
-else if((args[0] === 'get' && ['post', 'posts'].includes(args[1])) || args[0] === 'gp') {
+
+/*
+if((args[0] === 'get' && ['post', 'posts'].includes(args[1])) || args[0] === 'gp') {
   console.log('Get Post');
+  return;
 }
 else if((args[0] === 'get' && ['category', 'categories'].includes(args[1])) || args[0] === 'gc') {
   console.log('Get Category');
+  return;
 }
 else if(args[0] === 'get') {
   console.log('What Get?');
+  return;
 }
-else if(args[0] === 'post' && args[1] !== '') {
+
+if(args[0] === 'post' && args[1] !== '') {
   console.log('Post : ' + args[1]);
+  return;
 }
 else if(args[0] === 'post') {
   console.log('What Post?');
+  return;
 }
-else {
-  console.log('What Command?');
-}
+*/
+
+console.log('What Command?');
